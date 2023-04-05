8524c648cf89432ca8997eb6cd7cc1985452831c
vinloo <git-public.927fx@8shield.net>
2023-04-05 14:35:56 +0200

    initial code for unet and attention unet

---
+++ unet.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_c),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = ConvBlock(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, x):
+        s = self.conv(x)
+        p = self.pool(s)
+        return s, p
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = ConvBlock(out_c+out_c, out_c)
+
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.e1 = EncoderBlock(3, 64)
+        self.e2 = EncoderBlock(64, 128)
+        self.e3 = EncoderBlock(128, 256)
+        self.e4 = EncoderBlock(256, 512)
+
+        self.b = ConvBlock(512, 1024)
+
+        self.d1 = DecoderBlock(1024, 512)
+        self.d2 = DecoderBlock(512, 256)
+        self.d3 = DecoderBlock(256, 128)
+        self.d4 = DecoderBlock(128, 64)
+
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+
+    def forward(self, inputs):
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        b = self.b(p4)
+
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        outputs = self.outputs(d4)
+
+        return outputs
+
+
+class AttentionGate(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.Wg = nn.Sequential(
+            nn.Conv2d(in_c[0], out_c, kernel_size=1, padding=0),
+            nn.BatchNorm2d(out_c)
+        )
+        self.Ws = nn.Sequential(
+            nn.Conv2d(in_c[1], out_c, kernel_size=1, padding=0),
+            nn.BatchNorm2d(out_c)
+        )
+        self.relu = nn.ReLU(inplace=True)
+        self.output = nn.Sequential(
+            nn.Conv2d(out_c, out_c, kernel_size=1, padding=0),
+            nn.Sigmoid()
+        )
+
+    def forward(self, g, s):
+        Wg = self.Wg(g)
+        Ws = self.Ws(s)
+        out = self.relu(Wg + Ws)
+        out = self.output(out)
+        return out * s
+
+
+class AttentionDecoderBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+        self.ag = AttentionGate(in_c, out_c)
+        self.c1 = ConvBlock(in_c[0]+out_c, out_c)
+
+    def forward(self, x, s):
+        x = self.up(x)
+        s = self.ag(x, s)
+        x = torch.cat([x, s], axis=1)
+        x = self.c1(x)
+        return x
+
+
+class AttentionUNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.e1 = EncoderBlock(3, 64)
+        self.e2 = EncoderBlock(64, 128)
+        self.e3 = EncoderBlock(128, 256)
+
+        self.b1 = ConvBlock(256, 512)
+
+        self.d1 = AttentionDecoderBlock([512, 256], 256)
+        self.d2 = AttentionDecoderBlock([256, 128], 128)
+        self.d3 = AttentionDecoderBlock([128, 64], 64)
+
+        self.output = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        s1, p1 = self.e1(x)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+
+        b1 = self.b1(p3)
+
+        d1 = self.d1(b1, s3)
+        d2 = self.d2(d1, s2)
+        d3 = self.d3(d2, s1)
+
+        output = self.output(d3)
+        return output
+
+if __name__ == "__main__":
+    # inputs = torch.randn((2, 32, 256, 256))
+    # e = encoder_block(32, 64)
+    # x, p = e(inputs)
+    # print(x.shape, p.shape)
+    #
+    # d = decoder_block(64, 32)
+    # y = d(p, x)
+    # print(y.shape)
+
+    inputs = torch.randn((2, 3, 512, 512))
+    model = UNet()
+    y = model(inputs)
+    print(y.shape)
