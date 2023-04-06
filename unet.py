3470ae443031990b9ebd255ba537b6f131197b16
vinloo <git-public.927fx@8shield.net>
2023-04-06 10:23:40 +0200

    unet model with easy hyperparameter config

--- unet.py
+++ unet.py
@@ -1,16 +1,19 @@
 import torch
 import torch.nn as nn
 
+from dotmap import DotMap
+
 
 class ConvBlock(nn.Module):
-    def __init__(self, in_c, out_c):
+    """A convolutional block with two convolutional layers and batch normalization"""
+    def __init__(self, in_c, out_c, conf1, conf2):
         super().__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+            nn.Conv2d(in_c, out_c, kernel_size=conf1.k, padding=conf1.p, stride=conf1.s),
             nn.BatchNorm2d(out_c),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+            nn.Conv2d(out_c, out_c, kernel_size=conf2.k, padding=conf2.p, stride=conf2.s),
             nn.BatchNorm2d(out_c),
             nn.ReLU(inplace=True)
         )
@@ -20,11 +23,11 @@ class ConvBlock(nn.Module):
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, in_c, out_c):
+    def __init__(self, in_c, out_c, conf):
         super().__init__()
 
-        self.conv = ConvBlock(in_c, out_c)
-        self.pool = nn.MaxPool2d((2, 2))
+        self.conv = ConvBlock(in_c, out_c, conf.conv1, conf.conv2)
+        self.pool = nn.MaxPool2d((conf.pool_k, conf.pool_k))
 
     def forward(self, x):
         s = self.conv(x)
@@ -33,11 +36,11 @@ class EncoderBlock(nn.Module):
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_c, out_c):
+    def __init__(self, in_c, out_c, conf):
         super().__init__()
 
-        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
-        self.conv = ConvBlock(out_c+out_c, out_c)
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=conf.up.k, padding=conf.up.p, stride=conf.up.s)
+        self.conv = ConvBlock(out_c + out_c, out_c, conf.conv1, conf.conv2)
 
     def forward(self, inputs, skip):
         x = self.up(inputs)
@@ -48,21 +51,37 @@ class DecoderBlock(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self):
+    def __init__(self, conf: DotMap):
         super().__init__()
-        self.e1 = EncoderBlock(3, 64)
-        self.e2 = EncoderBlock(64, 128)
-        self.e3 = EncoderBlock(128, 256)
-        self.e4 = EncoderBlock(256, 512)
 
-        self.b = ConvBlock(512, 1024)
+        c_in = 1 if conf.grayscale else 3
 
-        self.d1 = DecoderBlock(1024, 512)
-        self.d2 = DecoderBlock(512, 256)
-        self.d3 = DecoderBlock(256, 128)
-        self.d4 = DecoderBlock(128, 64)
+        self.e1 = EncoderBlock(c_in, 64, conf.enc1)
+        self.e2 = EncoderBlock(64, 128, conf.enc2)
+        self.e3 = EncoderBlock(128, 256, conf.enc3)
+        self.e4 = EncoderBlock(256, 512, conf.enc4)
 
-        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+        self.b = ConvBlock(512, 1024, conf.b.conv1, conf.b.conv2)
+
+        self.d1 = DecoderBlock(1024, 512, conf.dec1)
+        self.d2 = DecoderBlock(512, 256, conf.dec2)
+        self.d3 = DecoderBlock(256, 128, conf.dec3)
+        self.d4 = DecoderBlock(128, 64, conf.dec4)
+
+        self.outputs = nn.Conv2d(64, 1, kernel_size=conf.out.k, padding=conf.out.p, stride=conf.out.s)
+
+
+    def compute_trf(self, x, y):
+        """Compute the theoretical receptive field of a pixel in the output"""
+        # TODO
+        pass
+
+
+    def compute_erf(self, x, y):
+        """Compute the effective receptive field of a pixel in the output"""
+        # TODO
+        pass
+
 
     def forward(self, inputs):
         s1, p1 = self.e1(inputs)
@@ -82,77 +101,77 @@ class UNet(nn.Module):
         return outputs
 
 
-class AttentionGate(nn.Module):
-    def __init__(self, in_c, out_c):
-        super().__init__()
+# class AttentionGate(nn.Module):
+#     def __init__(self, in_c, out_c):
+#         super().__init__()
 
-        self.Wg = nn.Sequential(
-            nn.Conv2d(in_c[0], out_c, kernel_size=1, padding=0),
-            nn.BatchNorm2d(out_c)
-        )
-        self.Ws = nn.Sequential(
-            nn.Conv2d(in_c[1], out_c, kernel_size=1, padding=0),
-            nn.BatchNorm2d(out_c)
-        )
-        self.relu = nn.ReLU(inplace=True)
-        self.output = nn.Sequential(
-            nn.Conv2d(out_c, out_c, kernel_size=1, padding=0),
-            nn.Sigmoid()
-        )
+#         self.Wg = nn.Sequential(
+#             nn.Conv2d(in_c[0], out_c, kernel_size=1, padding=0),
+#             nn.BatchNorm2d(out_c)
+#         )
+#         self.Ws = nn.Sequential(
+#             nn.Conv2d(in_c[1], out_c, kernel_size=1, padding=0),
+#             nn.BatchNorm2d(out_c)
+#         )
+#         self.relu = nn.ReLU(inplace=True)
+#         self.output = nn.Sequential(
+#             nn.Conv2d(out_c, out_c, kernel_size=1, padding=0),
+#             nn.Sigmoid()
+#         )
 
-    def forward(self, g, s):
-        Wg = self.Wg(g)
-        Ws = self.Ws(s)
-        out = self.relu(Wg + Ws)
-        out = self.output(out)
-        return out * s
-
-
-class AttentionDecoderBlock(nn.Module):
-    def __init__(self, in_c, out_c):
-        super().__init__()
-
-        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
-        self.ag = AttentionGate(in_c, out_c)
-        self.c1 = ConvBlock(in_c[0]+out_c, out_c)
-
-    def forward(self, x, s):
-        x = self.up(x)
-        s = self.ag(x, s)
-        x = torch.cat([x, s], axis=1)
-        x = self.c1(x)
-        return x
+#     def forward(self, g, s):
+#         Wg = self.Wg(g)
+#         Ws = self.Ws(s)
+#         out = self.relu(Wg + Ws)
+#         out = self.output(out)
+#         return out * s
 
 
-class AttentionUNet(nn.Module):
-    def __init__(self):
-        super().__init__()
+# class AttentionDecoderBlock(nn.Module):
+#     def __init__(self, in_c, out_c):
+#         super().__init__()
 
-        self.e1 = EncoderBlock(3, 64)
-        self.e2 = EncoderBlock(64, 128)
-        self.e3 = EncoderBlock(128, 256)
+#         self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+#         self.ag = AttentionGate(in_c, out_c)
+#         self.c1 = ConvBlock(in_c[0]+out_c, out_c)
 
-        self.b1 = ConvBlock(256, 512)
+#     def forward(self, x, s):
+#         x = self.up(x)
+#         s = self.ag(x, s)
+#         x = torch.cat([x, s], axis=1)
+#         x = self.c1(x)
+#         return x
 
-        self.d1 = AttentionDecoderBlock([512, 256], 256)
-        self.d2 = AttentionDecoderBlock([256, 128], 128)
-        self.d3 = AttentionDecoderBlock([128, 64], 64)
 
-        self.output = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+# class AttentionUNet(nn.Module):
+#     def __init__(self):
+#         super().__init__()
 
-    def forward(self, x):
-        s1, p1 = self.e1(x)
-        s2, p2 = self.e2(p1)
-        s3, p3 = self.e3(p2)
+#         self.e1 = EncoderBlock(3, 64)
+#         self.e2 = EncoderBlock(64, 128)
+#         self.e3 = EncoderBlock(128, 256)
 
-        b1 = self.b1(p3)
+#         self.b1 = ConvBlock(256, 512)
 
-        d1 = self.d1(b1, s3)
-        d2 = self.d2(d1, s2)
-        d3 = self.d3(d2, s1)
+#         self.d1 = AttentionDecoderBlock([512, 256], 256)
+#         self.d2 = AttentionDecoderBlock([256, 128], 128)
+#         self.d3 = AttentionDecoderBlock([128, 64], 64)
 
-        output = self.output(d3)
-        return output
+#         self.output = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+
+#     def forward(self, x):
+#         s1, p1 = self.e1(x)
+#         s2, p2 = self.e2(p1)
+#         s3, p3 = self.e3(p2)
+
+#         b1 = self.b1(p3)
+
+#         d1 = self.d1(b1, s3)
+#         d2 = self.d2(d1, s2)
+#         d3 = self.d3(d2, s1)
+
+#         output = self.output(d3)
+#         return output
 
 if __name__ == "__main__":
     # inputs = torch.randn((2, 32, 256, 256))
