75cf53e91f636649528e353b0a5c33b1c033d545
vinloo <git-public.927fx@8shield.net>
2023-04-23 14:21:09 +0200

    add all metrics, datasets and configurations to test script

--- test.py
+++ test.py
@@ -2,13 +2,19 @@ import torch
 import numpy as np
 import glob
 import os
+import pandas as pd
+from tqdm import tqdm
 from unet import UNet
 from torch.utils.data import DataLoader
 from utils.config import load_config
 from utils.metrics import dice_score, erf_rate_dataset, object_rate
 from utils.data import SegmentationDataset
+from preprocess_data import ALL_DATASETS
 
-def test_model(model, dataset_name, device="cuda"):
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+def test_model(model, configuration, dataset_name, device="cuda"):
     model.eval()
     
     # model_small_rf_by_hparams.load_state_dict(torch.load(f"results/small_rf_by_hparams/{dataset_name}_best_model.pt")["model_state_dict"])
@@ -22,8 +28,12 @@ def test_model(model, dataset_name, device="cuda"):
     erf_dist = np.zeros((576, 576, len(dataloader_test)))
     dice_scores = []
     object_rates = []
+    accuratcies = []
+    sensitivities = []
+    specificities = []
+    jaccard_scores = []
 
-    for i, (x, y) in enumerate(dataloader_test):
+    for i, (x, y) in tqdm(enumerate(dataloader_test), f"{dataset_name}/{configuration}", total=len(dataloader_test)):
         x.requires_grad = True
         x = x.to(device)
         y_true = y / 255
@@ -43,11 +53,39 @@ def test_model(model, dataset_name, device="cuda"):
         out = out.detach().cpu().numpy()
         out = (out >= 0.5).astype(int)
         out = np.squeeze(out)
+
+        # dice score
         dicescore = dice_score(out, np.squeeze(y.numpy()))
         dice_scores.append(dicescore)
         obectrate = object_rate(trf, np.squeeze(y.numpy()))
         object_rates.append(obectrate)
-    
+
+        # accuracy
+        accuracy = np.sum(out == np.squeeze(y.numpy())) / (576*576)
+        accuratcies.append(accuracy)
+
+        # sensitivity
+        numerator = np.sum((out == 1) & (np.squeeze(y.numpy()) == 1))
+        denominator = np.sum(np.squeeze(y.numpy()) == 1)
+        if denominator == 0:
+            sensitivity = 0
+        else:
+            sensitivity = numerator / denominator
+        sensitivities.append(sensitivity)
+
+        # specificity
+        specificity = np.sum((out == 0) & (np.squeeze(y.numpy()) == 0)) / np.sum(np.squeeze(y.numpy()) == 0)
+        specificities.append(specificity)
+
+        # jaccard score
+        numerator = np.sum((out == 1) & (np.squeeze(y.numpy()) == 1))
+        denominator = np.sum((out == 1) | (np.squeeze(y.numpy()) == 1))
+        if denominator == 0:
+            jaccard = 0
+        else:
+            jaccard = numerator / denominator
+        jaccard_scores.append(jaccard)
+
 
     # ERF rate
     erf = np.mean(erf_dist, axis=2)
@@ -60,19 +98,47 @@ def test_model(model, dataset_name, device="cuda"):
     # classification metrics
     dsc = np.mean(dice_scores)
     obj_rate = np.mean(object_rates)
+    acc = np.mean(accuratcies)
+    sens = np.mean(sensitivities)
+    spec = np.mean(specificities)
+    jacc = np.mean(jaccard_scores)
 
     return {
         "erf_rate": erf_rate,
         "dice_score": dsc,
-        "object_rate": obj_rate
+        "object_rate": obj_rate,
+        "accuracy": acc,
+        "sensitivity": sens,
+        "specificity": spec,
+        "jaccard_score": jacc
     }
 
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    config = load_config("default")
-    model = UNet(config).to(device)
-    model.load_state_dict(torch.load(f"out/fetal_head/default/best_model.pt")["model_state_dict"])
-    results = test_model(model, "fetal_head", device)
+    configurations = os.listdir("configurations")
+    configurations.remove("default.json")
+    configurations = [config[:-5] for config in configurations if config.endswith(".json")]
+    configurations.sort()
+    configurations = sorted(configurations, key=len)
+
+    results = []
+
+    for dataset_name in ALL_DATASETS:
+        dataset_results = pd.DataFrame(columns=configurations)
+        for config_name in configurations:
+            try:
+                config = load_config(config_name)
+                model = UNet(config).to(device)
+                model.load_state_dict(torch.load(f"out/{dataset_name}/{config_name}/best_model.pt")["model_state_dict"])
+                result = test_model(model, config_name, dataset_name, device)
+                dataset_results[config_name] = result
+            except FileNotFoundError:
+                print(f"Could not find files for: {dataset_name}/{config_name}. Skipping...")
+        results.append(dataset_results.copy())
+
+    results = pd.concat(results, axis=0, keys=ALL_DATASETS)
     print(results)
+    print("\nSaving results to results.csv...")
+    results.to_csv("results.csv")
